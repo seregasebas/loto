f95650835075ec33f78bb4bce269abc47e8c1f21
Big Sergey <seregasebas@gmail.com>
2022-05-29 20:35:13 +0700

    loto_game

--- loto_game.py
+++ loto_game.py
@@ -1,7 +1,19 @@
-import game
+import loto_card
 
-# value = input('Сколько будет игроков? 1/2: ')
+#перемешаем бочонки в мешке
+barrels = loto_card.Barrels.bochonki()
 
-game.game_one_user()
+value = int(input('Сколько будет человеческих игроков? 1/2: '))
+if value == 1:
+    #создадим карточки игрока и компьютера
+    user,computer = loto_card.Players.start_card()
+    #да начнется игра
+    loto_card.Start_game.game_one_user(barrels, user, computer)
+elif value == 2:
+    #создадим карточки игрокков и компьютеров
+    user_1,computer_1 = loto_card.Players.start_card()
+    user_2,computer_2 = loto_card.Players.start_card()
+    loto_card.Start_game.game_two_users(barrels, user_1, computer_1, user_2, computer_2)
+
 
 
