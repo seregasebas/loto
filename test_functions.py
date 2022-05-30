0f5298e5f14fd10df57462852ef215d3e7774f06
Sergey <seregasebas@gmail.com>
2022-05-30 15:55:28 +0700

    test_functions

--- test_functions.py
+++ test_functions.py
@@ -1,4 +1,22 @@
-import loto_card
+import loto_classes
 
-def test_random_numbers():
-    assert len(loto_card.Lotto_cards.random_numbers) == 15
+#проверка функции генерации бочонков
+def test_bochonki():
+    a = loto_classes.Barrels.bochonki()
+    assert len(a) == 90
+
+#проверка функции генерации карты
+def test_create_card():
+    a = loto_classes.Lotto_cards.create_card()
+    assert len(set(a)) == 16
+
+#проверка функции генерации карты
+def test_player_create_card():
+    a = loto_classes.Players.start_card_user()
+    assert len(set(a)) == 16
+
+#проверка функции генерации карты
+def test_comp_create_card():
+    a = loto_classes.Players.start_card_comp()
+    assert len(set(a)) == 16
+
