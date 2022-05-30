de06e1fd40c22613a77e8098e65b639a53118086
Sergey <seregasebas@gmail.com>
2022-05-30 15:55:19 +0700

    game

--- loto_game.py
+++ loto_game.py
@@ -1,19 +1,29 @@
-import loto_card
+import loto_classes
+import functions
 
 #перемешаем бочонки в мешке
-barrels = loto_card.Barrels.bochonki()
+barrels = loto_classes.Barrels.bochonki()
 
-value = int(input('Сколько будет человеческих игроков? 1/2: '))
-if value == 1:
-    #создадим карточки игрока и компьютера
-    user,computer = loto_card.Players.start_card()
-    #да начнется игра
-    loto_card.Start_game.game_one_user(barrels, user, computer)
-elif value == 2:
-    #создадим карточки игрокков и компьютеров
-    user_1,computer_1 = loto_card.Players.start_card()
-    user_2,computer_2 = loto_card.Players.start_card()
-    loto_card.Start_game.game_two_users(barrels, user_1, computer_1, user_2, computer_2)
+who_vs_who = int(input('1 - человек против человека/ 2 - человек против компьютера/ 3 - комп против компа 1/2/3?: '))
+
+#создадим карточки игрока(ов)    
+if who_vs_who == 1:
+    user = loto_classes.Players.start_card_user()
+    user_1 = loto_classes.Players.start_card_user()
+    #старт игры
+    loto_classes.Start_game.game_user_user(barrels, user, user_1)
+elif who_vs_who == 2:
+    user = loto_classes.Players.start_card_user()
+    computer = loto_classes.Players.start_card_comp()
+    #старт игры
+    loto_classes.Start_game.game_user_comp(barrels, user, computer)
+elif who_vs_who == 3:
+    computer = loto_classes.Players.start_card_comp()
+    computer_1 = loto_classes.Players.start_card_comp()
+    #старт игры
+    loto_classes.Start_game.game_comp_comp(barrels, computer, computer_1)
+
+
 
 
 
