0c1082e6fa0de09fa6b8fc97023c24ecb68402b3
Sergey <seregasebas@gmail.com>
2022-06-05 23:38:08 +0700

    py_test

--- test_py_classes.py
+++ test_py_classes.py
@@ -26,24 +26,26 @@ class TestBarrels():
 
     # выполняется в каждой последующей функции
     def setup(self):
-        self.a = loto_classes.Barrels.bochonki()
+        self.a = loto_classes.Barrels()
 
     #проверка функции генерации бочонков
     def test_bochonki(self):
-        assert len(self.a) == 90
+        assert len(self.a.bochonki()) == 90
 
-        #????????????
-    #ошибки pytest.raises
-    def test_bochonki_exception(self):
-        with pytest.raises(Exception):
-            assert len(self.a) == 91
+    #     #????????????
+    # #ошибки pytest.raises
+    # def test_bochonki_exception(self):
+    #     with pytest.raises(Exception):
+    #         assert len(self.a) == 91
 
 class TestPlayers():
 
-    def test_start_card_user(self):
-        self.user = loto_classes.Lotto_cards.create_card()
+    def test_create_card_player_1(self):
+        self.user = loto_classes.Players()
+        self.user = self.user.create_card()
         assert len(set(self.user)) == 16
 
-    def test_start_card_comp(self):
-        self.computer = loto_classes.Lotto_cards.create_card()
-        assert len(self.computer) == 27
+    def test_create_card_player_2(self):
+        self.user = loto_classes.Players()
+        self.user = self.user.create_card()
+        assert len(self.user) == 27
